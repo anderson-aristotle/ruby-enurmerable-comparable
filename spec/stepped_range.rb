5fc46128f70e9fa8b12dd78f6c72572ad6a2d329
jordanallain <jordan.allain@generalassemb.ly>
2018-02-13 12:30:28 -0500

    Add test for presence of each method
    - Add test for SteppedRange class to ensure it has an each method
      defined

--- spec/stepped_range.rb
+++ spec/stepped_range.rb
@@ -6,4 +6,8 @@ RSpec.describe 'SteppedRange class' do
   it 'includes the Enumerable module' do
     expect(SteppedRange.included_modules.include?(Enumerable)).to be true
   end
+
+  it "has an 'each' method defined" do
+    expect(SteppedRange.method_defined?(:each)).to be true
+  end
 end
