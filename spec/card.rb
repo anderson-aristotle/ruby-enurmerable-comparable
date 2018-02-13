ba8e42a30c14ac712145e56e6764b83f58913b6b
jordanallain <jordan.allain@generalassemb.ly>
2018-02-13 12:51:46 -0500

    Test that Card class includes Comparable module
    - Add test to check that the Card class has the Comparable mixin.

--- spec/card.rb
+++ spec/card.rb
@@ -3,4 +3,7 @@
 require_relative '../lib/card.rb'
 
 RSpec.describe 'Card class' do
+  it 'includes the Comparable module' do
+    expect(Card.included_modules.include?(Comparable)).to be true
+  end
 end
