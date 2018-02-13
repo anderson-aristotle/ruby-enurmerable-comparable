071411ac539c3f05a109fdfb09d3db971ad6d218
jordanallain <jordan.allain@generalassemb.ly>
2018-02-13 12:26:13 -0500

    Test that SteppedRange includes Enumerable
    - Add test to ensure that the Enumerable mixin is included.

--- spec/stepped_range.rb
+++ spec/stepped_range.rb
@@ -3,4 +3,7 @@
 require_relative '../lib/stepped_range.rb'
 
 RSpec.describe 'SteppedRange class' do
+  it 'includes the Enumerable module' do
+    expect(SteppedRange.included_modules.include?(Enumerable)).to be true
+  end
 end
