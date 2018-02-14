e05d40dfe4bc891abf223225ff199fc904c725ad
jordanallain <jordan.allain@generalassemb.ly>
2018-02-14 11:15:18 -0500

    Test for 'each' method in Deck class
    - Add test for presence of 'each' method in Deck class.

--- spec/deck_spec.rb
+++ spec/deck_spec.rb
@@ -6,4 +6,8 @@ RSpec.describe 'Deck class' do
   it 'includes the Enumerable module' do
     expect(Deck.included_modules.include?(Enumerable)).to be true
   end
+
+  it "has an 'each' method defined" do
+    expect(Deck.method_defined?(:each)).to be true
+  end
 end
