4c84ac2de46fd74df90e799b50448a743863bd9b
jordanallain <jordan.allain@generalassemb.ly>
2018-02-14 11:10:20 -0500

    Test for presence of Enumerable mixin in Deck
    - Add test for Deck class to ensure Enumerable mixin was used.

--- spec/deck_spec.rb
+++ spec/deck_spec.rb
@@ -3,4 +3,7 @@
 require_relative '../lib/deck.rb'
 
 RSpec.describe 'Deck class' do
+  it 'includes the Enumerable module' do
+    expect(Deck.included_modules.include?(Enumerable)).to be true
+  end
 end
